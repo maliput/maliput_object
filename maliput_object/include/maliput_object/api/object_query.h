aa0148a36e0e175fd370dfc15c9b364094337c50
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2022-04-27 13:30:07 -0300

    Adds ObjectQuery class. (#13)

---
+++ maliput_object/include/maliput_object/api/object_query.h
@@ -0,0 +1,74 @@
+// Copyright 2022 Toyota Research Institute
+#pragma once
+
+#include <optional>
+#include <vector>
+
+#include <maliput/api/lane.h>
+#include <maliput/api/road_network.h>
+#include <maliput/common/maliput_copyable.h>
+#include <maliput/math/vector.h>
+
+#include "maliput_object/api/object.h"
+#include "maliput_object/api/object_book.h"
+#include "maliput_object/api/overlapping_type.h"
+
+namespace maliput {
+namespace object {
+namespace api {
+
+/// Interface to perform queries on top of Maliput's RoadNetwork about Objects.
+/// To match convention of underlying RoadNetwork, the query interface use maliput::math::Vector3
+/// as the specialization of the Coordinate template argument.
+class ObjectQuery {
+ public:
+  MALIPUT_DEFAULT_COPY_AND_MOVE_AND_ASSIGN(ObjectQuery)
+
+  virtual ~ObjectQuery() = default;
+
+  /// Finds all the lanes overlapping with @p object .
+  /// @param object Object to find lanes overlapping with.
+  /// @returns A vector of pointers to lanes overlapping with @p object .
+  std::vector<const maliput::api::Lane*> FindOverlappingLanesIn(const Object<maliput::math::Vector3>* object) const {
+    return DoFindOverlappingLanesIn(object);
+  }
+
+  /// Finds all the lanes overlapping according the @p overlapping_type with @p object .
+  /// @param object Object to find lanes overlapping with.
+  /// @param overlapping_type Type of overlapping to find.
+  /// @returns A vector of pointers to lanes overlapping with @p object .
+  std::vector<const maliput::api::Lane*> FindOverlappingLanesIn(const Object<maliput::math::Vector3>* object,
+                                                                const OverlappingType& overlapping_type) const {
+    return DoFindOverlappingLanesIn(object, overlapping_type);
+  }
+
+  /// Finds the route between @p origin and @p target objects.
+  /// @param origin Object to find route from.
+  /// @param target Object to find route to.
+  std::optional<const maliput::api::LaneSRoute> Route(const Object<maliput::math::Vector3>* origin,
+                                                      const Object<maliput::math::Vector3>* target) const {
+    return DoRoute(origin, target);
+  }
+
+  /// @returns The ObjectBook.
+  const ObjectBook<maliput::math::Vector3>* object_book() const { return do_object_book(); }
+  /// @returns The maliput::api::RoadNetwork.
+  const maliput::api::RoadNetwork* road_network() const { return do_road_network(); }
+
+ protected:
+  ObjectQuery() = default;
+
+ private:
+  virtual std::vector<const maliput::api::Lane*> DoFindOverlappingLanesIn(
+      const Object<maliput::math::Vector3>* object) const = 0;
+  virtual std::vector<const maliput::api::Lane*> DoFindOverlappingLanesIn(
+      const Object<maliput::math::Vector3>* object, const OverlappingType& overlapping_type) const = 0;
+  virtual std::optional<const maliput::api::LaneSRoute> DoRoute(const Object<maliput::math::Vector3>* origin,
+                                                                const Object<maliput::math::Vector3>* target) const = 0;
+  virtual const ObjectBook<maliput::math::Vector3>* do_object_book() const = 0;
+  virtual const maliput::api::RoadNetwork* do_road_network() const = 0;
+};
+
+}  // namespace api
+}  // namespace object
+}  // namespace maliput
--- maliput_object/include/maliput_object/test_utilities/mock.h
+++ maliput_object/include/maliput_object/test_utilities/mock.h
@@ -1,14 +1,18 @@
 // Copyright 2022 Toyota Research Institute
 #include <memory>
 #include <unordered_map>
+#include <vector>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
+#include <maliput/api/lane.h>
+#include <maliput/api/road_network.h>
 #include <maliput/math/vector.h>
 
 #include "maliput_object/api/bounding_region.h"
 #include "maliput_object/api/object.h"
 #include "maliput_object/api/object_book.h"
+#include "maliput_object/api/object_query.h"
 #include "maliput_object/api/overlapping_type.h"
 
 namespace maliput {
@@ -33,6 +37,19 @@ class MockObjectBook : public api::ObjectBook<Coordinate> {
               (std::function<bool(const api::Object<Coordinate>*)>), (const, override));
   MOCK_METHOD((std::vector<api::Object<Coordinate>*>), DoFindOverlappingIn,
               (const api::BoundingRegion<Coordinate>&, const api::OverlappingType&), (const, override));
+};
+
+class MockObjectQuery : public api::ObjectQuery {
+ public:
+  MOCK_METHOD((std::vector<const maliput::api::Lane*>), DoFindOverlappingLanesIn,
+              (const api::Object<maliput::math::Vector3>*), (const, override));
+  MOCK_METHOD((std::vector<const maliput::api::Lane*>), DoFindOverlappingLanesIn,
+              (const api::Object<maliput::math::Vector3>*, const api::OverlappingType&), (const, override));
+  MOCK_METHOD((std::optional<const maliput::api::LaneSRoute>), DoRoute,
+              (const api::Object<maliput::math::Vector3>*, const api::Object<maliput::math::Vector3>*),
+              (const, override));
+  MOCK_METHOD((const api::ObjectBook<maliput::math::Vector3>*), do_object_book, (), (const, override));
+  MOCK_METHOD((const maliput::api::RoadNetwork*), do_road_network, (), (const, override));
 };
 
 }  // namespace test_utilities
